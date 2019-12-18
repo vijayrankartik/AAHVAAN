37a4006d54b5230cc9230e301dee7689d9020d3d
superstark02 <54995058+superstark02@users.noreply.github.com>
2019-12-18 15:39:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import App from './App';
+
+render(<App />, document.getElementById("root"))
---
+++ titleData.js
@@ -0,0 +1,27 @@
+import pic1 from './pic1.jpg';
+import pic2 from './pic2.jpg';
+import pic3 from './pic3.jpg';
+import pic4 from './pic4.jpg';
+const titleData = [
+    {
+      img: pic1,
+      cols:2,
+      title:'Event Name'
+    },
+    {
+      img: pic2,
+      cols:1,
+      title:'Event Name'
+    },
+    {
+      img: pic3,
+      cols:1,
+      title:'Event Name'
+    },
+    {
+      img: pic4,
+      cols:2,
+      title:'Event Name'
+    },
+  ];
+export default titleData;
