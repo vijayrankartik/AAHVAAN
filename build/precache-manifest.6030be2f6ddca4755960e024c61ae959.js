e2a211fcd4ff4f1f0c401b2f886dd178cbf39a84
superstark02 <54995058+superstark02@users.noreply.github.com>
2019-12-20 10:17:18 +0530

    Add files via upload

---
+++ build/precache-manifest.6030be2f6ddca4755960e024c61ae959.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34157cfe84c40337b2af391223277f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4258e5181466502e876",
+    "url": "/static/js/2.b5d862a8.chunk.js"
+  },
+  {
+    "revision": "29e65f23699e38c3bd8e",
+    "url": "/static/js/main.fd60d095.chunk.js"
+  },
+  {
+    "revision": "b3ef32d4c03e2747d4fc",
+    "url": "/static/js/runtime-main.8a71e9c7.js"
+  }
+]);
